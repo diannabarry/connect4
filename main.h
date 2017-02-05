9bce4ccb3ea1d59228f8f5b940a551f193bff0a6
diannabarry <diannab42@yahoo.com>
2017-02-05 13:46:18 -0500

    Create main.h

---
+++ main.h
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vectors>
+#include <strings>
+
+CONST int WIN_NUM = 4;
+CONST int BOARD_SIZE = 7;
+
+class Piece{
+  Private:
+    char state;
+  Public:
+    bool checkState(char);
+    bool setState(char);
+    char getState() const {return state;}
+    Piece(char);
+};
+
+int main(){
+  std::vector< std::vector <Piece> > V;
+  
+
+return 0;
+}
+
+Piece::Piece(char move){
+  if (setState(move) == true)
+    return;
+  else
+    state = 'e';
+}
+
+bool Piece::checkState(char move){ //returns true if move is by player 1 or 2. Otherwise returns false
+  if( move == '1')
+    return true;
+  else if(move == '2')
+    return true;
+  else
+    return false;
+}
+
+bool Piece::setState(char move){ //sets state to the move if the value is approved by checkState(). Does not set state otherwise
+  if (checkState(move) == true){
+    state = move;
+    return true;
+    }
+  else
+    return false;
+}
